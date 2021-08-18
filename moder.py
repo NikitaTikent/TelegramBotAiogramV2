755ec384abe87b4d038f11ccfd77e591619b3498
Nikita <81634393+NikitaTikent@users.noreply.github.com>
2021-08-18 19:48:51 +0500

    Delete moder.py

--- moder.py
+++
@@ -1,8 +0,0 @@
-def moderate(message):
-    user_name = message.from_user.first_name
-    text = message.text.lower()
-    mat = ['блять', 'сук', 'сука', 'пиздец', 'соси', 'хуйло', 'тварь', 'идиот', 'хуй', 'пенис']
-    for x in mat:
-        if x in text:
-            return True
-    return False
